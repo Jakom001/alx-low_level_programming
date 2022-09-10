8fd8569ca3c57e272e58494e037a99727c11593b
Peter <petremosh85@gmail.com>
2022-09-09 18:42:57 -0700

    to check positive or negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int n;
 
-	stand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -22,3 +22,4 @@ int main(void)
 		printf("%d is zero\n", n);
 
 	return (0);
+}
