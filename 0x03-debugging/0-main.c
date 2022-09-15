3c1335befba9af352fa437b6a249bed58c50d977
Peter <petremosh85@gmail.com>
2022-09-15 05:18:03 -0700

    Debug  c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * main - display the result of the function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_to_negative(i);
+
+	return (0);
+}
