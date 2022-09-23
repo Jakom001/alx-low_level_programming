8664453b715932c4416360b41c5e4f07fa22ea9e
Peter <petremosh85@gmail.com>
2022-09-23 12:52:13 -0700

    strings and arrays

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main."
 #include <stdio.h>
 
 /**
@@ -13,41 +13,44 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c = 0, b = 0, p, g, d, h, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + c) != '\0')
-		c++;
-	while (*(n2 + b) != '\0')
-		b++;
-	if (c >= b)
-		g = c;
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		g = b;
-	if (size_r <= g + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[g + 1] = '\0';
-	c--, b--, size_r--;
-	d = *(n1 + c) - 48, h = *(n2 + b) - 48;
-	while (g >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		p = d + h + add;
-		if (p >= 10)
-			add = p / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
 			add = 0;
-		if (p > 0)
-			*(r + g) = '0';
-		if (c > 0)
-			c--, d = *(n1 + c) - 48;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
 		else
-			d = 0;
-		if (b > 0)
-			b--, h = *(n2 + b) - 48;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			d = 0;
-		g--, size_r--;
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
 	else
-		return (r);}
+		return (r);
+}
