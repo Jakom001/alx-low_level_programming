656858b637377045a3b627d43064912ee2b6dcc3
Peter and Purity <petremosh85@gmail.com>
2022-10-24 13:44:15 -0700

    Singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t num;
 
-	nelem = 0;
+	num = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		num++;
 	}
-	return (nelem);
+	return (num);
 }
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -18,28 +18,6 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}#include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
-
-size_t print_list(const list_t *h)
-{
-	size_t num;
-
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		num++;
-	}
-	return (num);
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -48,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-endif
+#endif
